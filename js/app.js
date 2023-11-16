b97982f71a1b0cdc689f7fcf7613937939af1d07
VFrancischini <vfrancischini@outlook.com>
2023-11-16 06:51:51 -0300

    Funcionalidade Adicionar concluida

---
+++ js/app.js
@@ -0,0 +1,41 @@
+let listaDeAmigos = [];
+
+function adicionar() {
+    let nomeAmigo = document.getElementById('nome-amigo').value;
+    let amigosAdicionados = document.getElementById('lista-amigos');
+
+    listaDeAmigos.push = nomeAmigo;
+
+    if (amigosAdicionados.textContent == '') {
+        amigosAdicionados.textContent = nomeAmigo;
+    } else {
+        amigosAdicionados.textContent += ', ' + nomeAmigo;
+    }
+}
+
+function sortear() {
+    embaralha(listaDeAmigos);
+
+    let listaSorteio = document.getElementById('lista-sorteio');
+
+    for (let indice = 0; indice < listaDeAmigos.length; indice++) {
+
+        if (indice == listaDeAmigos.length - 1) {
+            listaSorteio.innerHTML += listaDeAmigos[indice] + ' --> ' + listaDeAmigos[0] + '<br>';
+        } else {
+            listaSorteio.innerHTML += listaDeAmigos[indice] + ' --> ' + listaDeAmigos[indice + 1] + '<br>';
+        }
+    }
+}
+
+function reiniciar() {
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
